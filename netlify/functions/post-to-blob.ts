73102fedc96ef2777c24016f86bb33921db1730e
Sean Mc Ternan <seanmcternan@gmail.com>
2024-09-04 20:40:17 -0600

    Add Blobs

--- netlify/functions/post-to-blob.ts
+++ netlify/functions/post-to-blob.ts
@@ -1,16 +1,17 @@
 import { Handler } from '@netlify/functions';
 import qs from 'qs';
+import { getStore } from "@netlify/blobs";
 
 const handler: Handler = async (event, context) => {
     if (event.httpMethod === 'POST') {
-        console.log("I got a post");
 
         // Parse the URL-encoded form data
         const formData = qs.parse(event.body);
 
         // Log the parsed form data
-        console.log("Form values:", formData);
-
+        console.log("Form values:", formData.data);
+        const levels = getStore("levels");
+        // await levels.setJSON("nails", { type: "common", finish: "bright" });
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Post received" }),
