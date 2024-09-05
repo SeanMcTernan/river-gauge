2d920a9f91e952dfafab37a7c34b5bd60887fd74
Sean Mc Ternan <seanmcternan@gmail.com>
2024-09-04 20:50:31 -0400

    log values

---
+++ netlify/functions/post-to-blob.ts
@@ -0,0 +1,26 @@
+import { Handler } from '@netlify/functions';
+import qs from 'qs';
+
+const handler: Handler = async (event, context) => {
+    if (event.httpMethod === 'POST') {
+        console.log("I got a post");
+
+        // Parse the URL-encoded form data
+        const formData = qs.parse(event.body);
+
+        // Log the parsed form data
+        console.log("Form values:", formData);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Post received" }),
+        };
+    }
+
+    return {
+        statusCode: 405,
+        body: JSON.stringify({ message: "Method Not Allowed" }),
+    };
+};
+
+export { handler };
